088210401ac2483352055f41c2c406e83df62c47
TonyCross23 <tonychamp40@gmail.com>
2025-02-17 19:10:04 +0630

    skill

--- src/features/skills/skill.route.js
+++ src/features/skills/skill.route.js
@@ -8,12 +8,31 @@ import {
 } from "./skill.controller.js";
 import validate from "../../middleware/validate.js";
 import { skillSchema } from "./skill.validation.js";
+import authenticateToken from "../../middlewares/authMiddleware.js";
+import adminMiddleware from "../../middlewares/adminMiddleware.js";
 
 const skillRouter = express.Router();
 
-skillRouter.post("/", validate(skillSchema), skillCreateController);
-skillRouter.get("/", getSkillsControllr);
-skillRouter.get("/:id", getSkillByIdControler);
-skillRouter.put("/:id", validate(skillSchema), updateSkillController);
-skillRouter.delete("/:id", destorySkillController);
+skillRouter.post(
+  "/",
+  validate(skillSchema),
+  authenticateToken,
+  adminMiddleware,
+  skillCreateController
+);
+skillRouter.get("/", authenticateToken, getSkillsControllr);
+skillRouter.get("/:id", authenticateToken, getSkillByIdControler);
+skillRouter.put(
+  "/:id",
+  validate(skillSchema),
+  authenticateToken,
+  adminMiddleware,
+  updateSkillController
+);
+skillRouter.delete(
+  "/:id",
+  authenticateToken,
+  adminMiddleware,
+  destorySkillController
+);
 export default skillRouter;
