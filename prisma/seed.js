737e4421048cabfe7b1b6c322d8d54b9134ce0ed
Paing0 <painghtoomyat007@gmail.com>
2025-03-14 12:43:33 +0000

    cleanup

--- prisma/seed.js
+++ prisma/seed.js
@@ -11,21 +11,6 @@ async function main() {
   await locationSeeder();
   await skillSeeder();
   await jobCategoriesSeeder();
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  //await companyProfileSeeder();
->>>>>>> a107096add5e70b6a84edd8623bf490de7a17b4c
-=======
-
->>>>>>> 67073bac3e9648509c07bc47bb7900ce55fe6e4a
-=======
-
->>>>>>> 7b0e60d2298a1a848d13933e8b51abaaf10c4758
-=======
->>>>>>> d152c4b57f09b8c2ae55ec1d45d5696954f24893
 }
 
 main()
